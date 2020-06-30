50da595551c9faefc7df902b5bf7c0de0fb440b4
Daniel Plohmann (jupiter) <plohmann@informatik.uni-bonn.de>
2020-06-30 13:43:47 +0200

    fixed link in setup py

--- setup.py
+++ setup.py
@@ -7,13 +7,13 @@ with open("README.md", "r") as fh:
 
 setup(
     name='apiscout',
-    version='1.1.0',
+    version='1.1.1',
     description='A library for Windows API usage recovery and similarity assessment with focus on memory dumps.',
     long_description_content_type="text/markdown",
     long_description=long_description,
     author='Daniel Plohmann',
     author_email='daniel.plohmann@mailbox.org',
-    url='https://github.com/daniel-plohmann/apiscout',
+    url='https://github.com/danielplohmann/apiscout',
     license="BSD 2-Clause",
     packages=find_packages(exclude=('tests', 'dbs')),
     package_data={'apiscout': ['data/winapi1024v1.txt', 'data/winapi_contexts.csv', 'data/html_frame.html']},
